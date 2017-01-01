02241407963186ddee38e67278be26e99747ac5e
liulupeng <liulupeng3385@163.com>
2017-01-01 09:59:09 +0800

    add validation to title

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,13 +7,6 @@ class GroupsController < ApplicationController
     @group = Group.new
   end
 
-  def create
-    @group = Group.new(group_params)
-    @group.save
-
-      redirect_to groups_path
-  end
-
   def show
     @group = Group.find(params[:id])
   end
@@ -21,6 +14,33 @@ class GroupsController < ApplicationController
   def edit
     @group = Group.find(params[:id])
   end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to groups_path, notice: 'Update Success'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    flash[:alert] = "Group delated"
+    redirect_to groups_path
+  end
+
+
 
   private
 
